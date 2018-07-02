1f4fa64b6961865e0ef2e17e2ba74c9d79fa74c6
crossphd <cross25@uis.edu>
2018-07-02 13:30:59 -0700

    added details page updates and new settings menu that allows user to sort by either popular movies or highest rated

--- app/src/main/java/com/crossphd/popularmovies/MainActivity.java
+++ app/src/main/java/com/crossphd/popularmovies/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,14 +25,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>> {
-
-//    video json response sample url: https://api.themoviedb.org/3/movie/499772/videos?api_key=3636e8f94c49efca06551bbd383c9484
+public class MainActivity
+        extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<Movie>>,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static String BUILT_URL ="https://api.themoviedb.org/3/movie/popular?api_key=3636e8f94c49efca06551bbd383c9484";
-    private static String POPULAR_URL = "https://api.themoviedb.org/3/discover/movie?api_key=3636e8f94c49efca06551bbd383c9484&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&page=1";
-    private static String RATING_URL = "https://api.themoviedb.org/3/discover/movie?api_key=3636e8f94c49efca06551bbd383c9484&language=en-US&region=US&sort_by=vote_average.desc&include_adult=false&page=1";
+    private static String BUILT_URL ="";
+    String API_KEY = BuildConfig.MY_MOVIE_DB_API_KEY;
+    private String POPULAR_URL = String.format("https://api.themoviedb.org/3/discover/movie?api_key=%s&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&page=1", API_KEY);
+    private String RATING_URL = String.format("https://api.themoviedb.org/3/discover/movie?api_key=%s&language=en-US&region=US&sort_by=vote_average.desc&include_adult=false&page=1", API_KEY);
     private MovieAdapter adapter;
     private TextView mEmptyStateTextView;
     private static final int ARTICLE_LOADER_ID = 1;
@@ -38,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setUpSharedPreferences();
+
         setContentView(R.layout.activity_main);
 
         final GridView movieGridView = findViewById(R.id.movies_grid);
@@ -71,6 +78,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setUpSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String sort = sharedPreferences.getString("sort_order", null);
+//        Log.v(LOG_TAG, "******************  shared preferences sort order = " + sort + " ********************");
+//        Log.v(LOG_TAG, "******************  APIKEY = " + API_KEY + " ********************");
+        if (sort.equals("1")) {
+            BUILT_URL = POPULAR_URL;
+        }
+        else if (sort.equals("2")) {
+            BUILT_URL = RATING_URL;
+        }
+//        else {
+//            BUILT_URL = POPULAR_URL;
+//        }
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+    }
+
     private void launchDetailActivity(Movie m) {
         Intent intent = new Intent(MainActivity.this, MovieDetail.class);
         intent.putExtra("movie", m);
@@ -79,20 +111,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
             return true;
         }
 
@@ -102,21 +131,18 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private void update() {
         LoaderManager loaderManager = getLoaderManager();
         loaderManager.restartLoader(ARTICLE_LOADER_ID, null, this);
-//        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public Loader<List<Movie>> onCreateLoader(int id, Bundle args) {
         Log.v(LOG_TAG, "onCreateLoader called");
-        return new MovieLoader(this, POPULAR_URL);
+        return new MovieLoader(this, BUILT_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> movies) {
         adapter.clear();
         Log.v(LOG_TAG, "OnLoadFinished called");
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
         if (movies != null && !movies.isEmpty()) {
             adapter.addAll(movies);
         }
@@ -125,5 +151,20 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<Movie>> loader) {
         adapter.clear();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_sort_order_key))) {
+            String sort = sharedPreferences.getString(key, null);
+//            Log.v(LOG_TAG, "******************  shared preferences sort order = " + sort + " ********************");
+            if (sort.equals("1")) {
+                BUILT_URL = POPULAR_URL;
+            }
+            else if (sort.equals("2")) {
+                BUILT_URL = RATING_URL;
+            }
+        }
+        update();
     }
 }
